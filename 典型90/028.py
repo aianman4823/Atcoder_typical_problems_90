bded0a9e5d2d40a762165140fa3a2e14aa499de1
aianman4823 <akitoaianman1@gmail.com>
2022-03-13 16:47:40 +0900

    028追加 2次元いもす法のチップスを追加

---
+++ 典型90/028.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+# 二次元いもす法
+N = int(input())
+A = [[0] * 1100 for _ in range(1100)]
+
+for _ in range(N):
+    lx, ly, rx, ry = map(int, input().split())
+    A[lx][ly] += 1
+    A[rx][ry] += 1
+    A[rx][ly] += -1
+    A[lx][ry] += -1
+
+for i in range(1100):
+    for j in range(1100):
+        if i == 0 and j == 0:
+            continue
+        A[i][j] = A[i][j] + A[i][j - 1]
+
+
+for i in range(1100):
+    for j in range(1100):
+        if i == 0 and j == 0:
+            continue
+        A[j][i] = A[j][i] + A[j - 1][i]
+
+
+count = defaultdict(int)
+for i in range(1100):
+    for j in range(1100):
+        count[A[i][j]] += 1
+
+for i in range(N):
+    print(count[i + 1])
