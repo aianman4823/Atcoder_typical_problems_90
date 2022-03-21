afffec46e2d8825e5d980e82cc34a5867faa68ea
aianman4823 <akitoaianman1@gmail.com>
2022-03-21 14:12:56 +0900

    032追加

---
+++ 典型90/032.py
@@ -0,0 +1,13 @@
+N = int(input())
+A = []
+for _ in range(N):
+    a = list(map(int, input().split()))
+    A.append(a)
+
+M = int(input())
+uwasa = []
+for _ in range(M):
+    x, y = map(int, input().split())
+    uwasa.append([x, y])
+
+
